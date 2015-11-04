d3cd86fafa602bb7d87cf259cfdab5cc322f8ca9
eatingNumbers <deanlaurea@icloud.com>
2015-11-03 19:25:12 -0600

    Compare inheritance and protocols

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -23,3 +23,45 @@ struct Friend: FullyNamed {
 let friend = Friend(firstName:
     "Dean", middleName: "Villena", lastName: "Laurea")
 friend.fullName
+
+// Inheritance vs. Protocols
+
+import Foundation
+
+class Employee {
+    let name: String
+    let address: String
+    let startDate: NSDate
+    let type: String
+    
+    var department: String?
+    var reportsTo: Employee?
+    
+    init(fullName: String, employeeAddress: String, employeeStartDate: NSDate, employeeType: String) {
+        name = fullName
+        address = employeeAddress
+        startDate = employeeStartDate
+        type = employeeType
+    }
+    
+    func pay() -> (basePay: Int, benefits: Int, deductions: Int, vacationTime: Int) {
+        return (0,0,0,0)
+    }
+}
+
+class HourlyEmployee: Employee {
+    
+    let hourlyWage = 12
+    let hoursWorked = 40
+    let availableVacation = 0
+    
+    override func pay() -> (basePay: Int, benefits: Int, deductions: Int, vacationTime: Int) {
+        return (hourlyWage * hoursWorked, 0, 0, availableVacation)
+    }
+}
+
+class SalariedEmployee: Employee {
+    
+    let salary = 40000
+    
+}
