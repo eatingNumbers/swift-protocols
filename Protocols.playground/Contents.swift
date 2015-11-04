2a09736c1214ae4ad45007df7bfcbd676800c852
eatingNumbers <deanlaurea@icloud.com>
2015-11-03 19:11:52 -0600

    Modify repo to exclue user state and learn protocols

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -1,2 +1,25 @@
 // Protocols
 
+protocol FullyNamed {
+    var fullName:String { get }
+}
+
+struct User: FullyNamed {
+    var fullName: String
+}
+
+let user = User(fullName: "Dean Laurea")
+
+struct Friend: FullyNamed {
+    var firstName: String
+    var middleName: String
+    var lastName: String
+    
+    var fullName:String {
+        return "\(firstName)\(middleName)\(lastName)"
+    }
+}
+
+let friend = Friend(firstName:
+    "Dean", middleName: "Villena", lastName: "Laurea")
+friend.fullName
